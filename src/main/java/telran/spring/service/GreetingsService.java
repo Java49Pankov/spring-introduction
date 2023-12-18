0a449a0dc4b972da1cab5832c6762fd57fe3d551
Evgeny Pankov <volvovnl123@gmail.com>
2023-12-18 10:59:24 +0700

    CW #57 CRUD Restful service

--- src/main/java/telran/spring/service/GreetingsService.java
+++ src/main/java/telran/spring/service/GreetingsService.java
@@ -1,5 +1,21 @@
 package telran.spring.service;
 
+import telran.spring.Person;
+
 public interface GreetingsService {
 	String getGreetings(long id);
+
+	// TODO add for HW #57
+	// Person getPerson(long id);
+	// List<Person> getPersonsByCity(String city);
+
+	// TODO update for HW #57
+	// Person addPerson(Person person);
+	// Person deletePerson(long id);
+	// Person updatePerson(Person person);
+	String addName(IdName idName);
+
+	String deleteName(long id);
+
+	String updateName(IdName idName);
 }
