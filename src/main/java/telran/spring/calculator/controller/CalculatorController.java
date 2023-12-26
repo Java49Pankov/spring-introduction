57049ec2d27ce3a61ea3b36ac668fd8abeaf83df
Evgeny Pankov <volvovnl123@gmail.com>
2023-12-26 21:46:50 +0700

    CW #61 adds initial code with ArithmeticCalculatorService

---
+++ src/main/java/telran/spring/calculator/controller/CalculatorController.java
@@ -0,0 +1,40 @@
+package telran.spring.calculator.controller;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import telran.exceptions.NotFoundException;
+import telran.spring.calculator.dto.OperationData;
+import telran.spring.calculator.service.CalculatorService;
+
+@RestController
+@RequestMapping("calculator")
+@RequiredArgsConstructor
+public class CalculatorController {
+	final List<CalculatorService> calculatorServices;
+	Map<String, CalculatorService> servicesMap;
+
+	@PostConstruct
+	void createServicesMap() {
+		servicesMap = calculatorServices
+				.stream()
+				.collect(Collectors
+						.toMap(service -> service.getCalculationType(), service -> service));
+	}
+
+	@PostMapping
+	String calculate(@RequestBody @Valid OperationData operationData) {
+		String type = operationData.type();
+		CalculatorService calculatorService = servicesMap.get(type);
+		if (calculatorService == null) {
+			throw new NotFoundException(String.format("type %s not found", type));
+		}
+		return calculatorService.calculate(operationData);
+	}
+}
---
+++ src/main/java/telran/spring/calculator/service/CalculatorService.java
@@ -0,0 +1,10 @@
+package telran.spring.calculator.service;
+
+import telran.spring.calculator.dto.OperationData;
+
+public interface CalculatorService {
+
+	String calculate(OperationData operationData);
+
+	String getCalculationType();
+}
